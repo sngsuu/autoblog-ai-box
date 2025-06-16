9eafcf8a64100a760ed5e6d0a514c9f311ffbd80
haseungsoo <sngsuu@gmail.com>
2025-06-16 17:03:22 +0900

    fix: 카테고리별 블로그 글이 표시되도록 content 컬렉션 구성 추가

---
+++ src/content/config.ts
@@ -0,0 +1,14 @@
+import { defineCollection, z } from "astro:content";
+
+const blog = defineCollection({
+  type: "content",
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    pubDate: z.date()
+  }),
+});
+
+export const collections = {
+  blog,
+};
